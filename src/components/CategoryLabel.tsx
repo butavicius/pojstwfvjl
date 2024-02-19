39a828c69f6f3761a14ccdffd24365ff9544fa55
Simas Butavičius <simasbutavicius@gmail.com>
2024-02-19 14:03:56 +0100

    Don't show category ID

--- src/components/CategoryLabel.tsx
+++ src/components/CategoryLabel.tsx
@@ -1,7 +1,7 @@
 import {CategoryLabelProps} from "../category-tree/types";
 
-export default function CategoryLabel({id, name, depth}: CategoryLabelProps) {
+export default function CategoryLabel({name, depth}: CategoryLabelProps) {
    const labelText = `${"- ".repeat(depth)}${name}`;
 
-   return <div>{labelText} - {id}</div>
+   return <h4>{labelText}</h4>
 }
